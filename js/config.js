e223c4e502249b7dca44f3fe2a863e5713ca0afa
Ajay Kumar <ajay@wayone.co.in>
2026-03-27 16:27:15 +0530

    added QR code

--- js/config.js
+++ js/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     // Development: http://192.168.1.48:5019/api
     // Production: https://api.getmyphysio.in/api
     API_BASE_URL: 'https://api.getmyphysio.in/api',
-    
+
     // Blog API Endpoints
     BLOGS: {
         // Get all public blogs - Returns list of all published blogs
@@ -16,19 +16,19 @@ const CONFIG = {
         // Get single blog by slug - Returns specific blog details
         PUBLIC_DETAIL: '/blogs/public'
     },
-    
+
     // Helper function to get full API URL
-    getUrl: function(endpoint) {
+    getUrl: function (endpoint) {
         return this.API_BASE_URL + endpoint;
     },
-    
+
     // Helper function to get blog list URL
-    getBlogListUrl: function() {
+    getBlogListUrl: function () {
         return this.getUrl(this.BLOGS.PUBLIC_LIST);
     },
-    
+
     // Helper function to get blog detail URL
-    getBlogDetailUrl: function(slug) {
+    getBlogDetailUrl: function (slug) {
         return this.getUrl(this.BLOGS.PUBLIC_DETAIL) + '/' + slug;
     }
 };
